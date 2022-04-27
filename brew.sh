028f95dc88baddfbae4c0623be06c5d63e31dc79
David Furman <DavidAsherFurman@gmail.com>
2022-04-27 12:51:04 -0700

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -12,6 +12,10 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 # Install if we don't have it
 if test ! $(which brew); then
   echo "Installing homebrew..."
+  
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/dafurman/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+  
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
